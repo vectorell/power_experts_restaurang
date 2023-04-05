b1897ecbf41c667f7320b3bd040365203e0aca60
QueAnka <annika.nylin@hotmail.com>
2023-04-05 16:04:52 +0200

    first commit for header

---
+++ power_experts_restaurang/src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+
+const Header = () => {
+
+return(
+
+	<section>
+		<img src="src\components\images\logo-no-background 1.svg" alt="Företagslogga Feast & Fare" />
+		<img src="" alt="Varukorg" />
+		<img src="" alt="Länkar till olika sidor"/>
+
+	</section>
+)
+
+}
