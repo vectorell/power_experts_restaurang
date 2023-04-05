723c4a1c0f085df336c76f0bd7c9b21cb5f69723
vectorell <vlindell@outlook.com>
2023-04-05 13:39:01 +0200

    restructured files

--- power_experts_restaurang/src/App.jsx
+++ power_experts_restaurang/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Varukorg from './components/Varukorg/Varukorg'
+import Varukorg from './components/Varukorg'
 import './base.css'
 
 function App() {
