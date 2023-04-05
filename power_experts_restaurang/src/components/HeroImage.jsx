5777b4e5f1fe4102e5ad96caf4f743d77a8f3a3f
QueAnka <annika.nylin@hotmail.com>
2023-04-05 11:54:14 +0200

    positioned img and button

--- power_experts_restaurang/src/components/HeroImage.jsx
+++ power_experts_restaurang/src/components/HeroImage.jsx
@@ -6,8 +6,8 @@ const HeroImage = () => {
 
 	return(
 		
-		<section className= 'hero-image'>
-{/* <img src="" alt="Interiörbild från restaurangen" /> */}
+		<section className= 'hero-image-container'>
+<img src="src\components\images\pexels-volkan-vardar-3887985.jpg" alt="Interiörbild från restaurangen" className='hero-image'  />
 <button className='menu-button' >Till menyn</button>
 		</section>
 	)
