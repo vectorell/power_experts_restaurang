c3055d24ac166df91f83a05621e13f146ff93876
QueAnka <annika.nylin@hotmail.com>
2023-04-06 11:12:47 +0200

    header layout

--- power_experts_restaurang/src/components/Header.jsx
+++ power_experts_restaurang/src/components/Header.jsx
@@ -1,16 +1,44 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons"
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import './Header.css'
 
 const Header = () => {
+
+const OnClickCart =() =>{
+	console.log('jag klickade på varukorg');
+}
+
+const OnClickBars =() =>{
+	console.log('jag klickade på hamurgare');
+}
+
+const onClickHome = () => {
+	console.log('jag vill hem!');
+}
 
 return(
 
 	<section className='header'>
-		<img src="src\components\images\logo-no-background 1.svg" alt="Företagslogga Feast & Fare" />
-		<FontAwesomeIcon icon = {faCartShopping} className='cart'/>
-		<FontAwesomeIcon icon = {faBars}/>
 
+		<div className="header-mobile">
+			<img src="src\components\images\logo-no-background 1.svg" alt="Företagslogga Feast & Fare" onClick={onClickHome}/>
+			<FontAwesomeIcon icon = {faShoppingBasket } className='bars fa-lg'/>
+			<FontAwesomeIcon icon = {faCartShopping} className='cart fa-lg' onClick={OnClickCart}/>
+			<FontAwesomeIcon icon = {faBars} className='bars fa-lg' onClick={OnClickBars}/>
+		</div>
+		<div className='header-desktop'>
+		<img src="src\components\images\logo-no-background 1.svg" alt="Företagslogga Feast & Fare" onClick={onClickHome} className="logo-desktop"/>
+			<p> Till menyn</p>
+			<p>Personal</p>
+				<div className="phone">
+					<FontAwesomeIcon icon = {faPhone}/>
+					<p>0700-202020</p>
+				</div>
+			</div>
 	</section>
 )
 
