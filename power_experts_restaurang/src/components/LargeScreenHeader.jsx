ac76e4fcd3001227284689cbfd60378dc65c5227
QueAnka <annika.nylin@hotmail.com>
2023-04-13 10:31:33 +0200

    added keyboard event for links

--- power_experts_restaurang/src/components/LargeScreenHeader.jsx
+++ power_experts_restaurang/src/components/LargeScreenHeader.jsx
@@ -13,11 +13,18 @@ const LargeScreenHeader = () => {
 		console.log('jag klickade på varukorg');
 		navigateTo('varukorg')
 	}
+	const onKeyDownCart = () => {
+		{event.key === 'Enter' ? navigateTo('varukorg') : null}
+	}
+
 
 	// gå till landingpage
 	const onClickHome = () => {
 		console.log('jag vill hem!');
 		navigateTo('landing')
+	}
+	const onKeyDownHome = () => {
+		{event.key === 'Enter' ? navigateTo('landing') : null}
 	}
 
 	// gå till menyn
@@ -25,25 +32,31 @@ const LargeScreenHeader = () => {
 		console.log('Jag vill se menyn');
 		navigateTo('menu')
 	}
+	const onKeyDownMenu = () => {
+		{event.key === 'Enter' ? navigateTo('menu') : null}
+	}
 
 	// gå till personal login
 	const onClickLogin = () => {
 		console.log('personal login');
 	}
+	const onKeyDownLogin = () => {
+		{event.key === 'Enter' ? console.log('personal login') : null}
+	}
 
 
 	return(
 	<div className='header-desktop'>
-		<img src="src\components\images\logo-no-background 1.png" alt="Företagslogga Feast & Fare" onClick={onClickHome} className="header-logo" tabIndex={0}/>
+		<img src="src\components\images\logo-no-background 1.png" alt="Företagslogga Feast & Fare" onClick={onClickHome} onKeyDown={onKeyDownHome} className="header-logo" tabIndex={0}/>
 		<ul className="main-menu  desktop-main-menu" >
-			<li onClick ={onClickMenu} className='link' tabIndex={0}> Till menyn</li>
-			<li onClick={onClickLogin} className="link" tabIndex={0}>Personal</li>
+			<li onClick ={onClickMenu} onKeyDown={onKeyDownMenu} className='link' tabIndex={0}> Till menyn</li>
+			<li onClick={onClickLogin} onKeyDown={onClickLogin} className="link" tabIndex={0}>Personal</li>
 				<li className="phone">
 					<FontAwesomeIcon icon = {faPhone} className="phone-icon"/>
 					<p tabIndex={0}>0700-202020</p>
 				</li>
 			</ul>		
-			<FontAwesomeIcon icon = {faShoppingBasket} className='cart cart-desktop link fa-lg' onClick={onClickCart} aria-label='Varukorg' />
+			<FontAwesomeIcon icon = {faShoppingBasket} className='cart cart-desktop link fa-lg' onClick={onClickCart} onKeyDown={onKeyDownCart} aria-label='Varukorg' />
 
 			</div>
 	)		
