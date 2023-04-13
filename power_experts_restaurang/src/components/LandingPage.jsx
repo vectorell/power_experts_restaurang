b603dcbe068606a99aeab68cb6a060bba6ea01b3
QueAnka <annika.nylin@hotmail.com>
2023-04-13 15:10:12 +0200

    rebuilt landing-page

---
+++ power_experts_restaurang/src/components/LandingPage.jsx
@@ -0,0 +1,16 @@
+import OpeningHours from "./OpeningHours"
+import HeroImage from "./HeroImage"
+
+
+const LandingPage = () => {
+
+	return(
+		<section className='landing-page'>
+			<HeroImage/>
+			<OpeningHours/>
+ 		</section>
+	)
+	
+}
+
+export default LandingPage
