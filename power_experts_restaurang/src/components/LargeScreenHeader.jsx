ce05b887f95f4b5822b5905e8653f14c47607e5b
Mustafa <mustafazeinali@gmail.com>
2023-04-13 16:15:12 +0200

    text-under cart dyker upp för bestänning

--- power_experts_restaurang/src/components/LargeScreenHeader.jsx
+++ power_experts_restaurang/src/components/LargeScreenHeader.jsx
@@ -1,66 +1,116 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ContextProvider } from "../App";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
+const TextForCartShopping = () => {
+  return <div className="text-under-cart"> beställt </div>;
+};
 
 const LargeScreenHeader = () => {
-	const { navigateTo } = useContext(ContextProvider)
+  const { navigateTo, selectedFoodItems } = useContext(ContextProvider);
+  const [areItemsInCart, setAreItemsInCart] = useState(false);
 
-	// gå till varukorg
-	const onClickCart =() =>{
-		console.log('jag klickade på varukorg');
-		navigateTo('varukorg')
-	}
-	const onKeyDownCart = () => {
-		{event.key === 'Enter' ? navigateTo('varukorg') : null}
-	}
+  // lyssna på när selectedfooditems uppdateras:
+  useEffect(() => {
+    if (selectedFoodItems.length > 0) {
+      console.log("Du har beställt");
+	
+      setAreItemsInCart(true);
+    } else {
+      setAreItemsInCart(false);
+    }
+  }, [selectedFoodItems]);
 
+  // gå till varukorg
+  const onClickCart = () => {
+    console.log("jag klickade på varukorg");
+    navigateTo("varukorg");
+  };
+  const onKeyDownCart = () => {
+    {
+      event.key === "Enter" ? navigateTo("varukorg") : null;
+    }
+  };
 
-	// gå till landingpage
-	const onClickHome = () => {
-		console.log('jag vill hem!');
-		navigateTo('landing')
-	}
-	const onKeyDownHome = () => {
-		{event.key === 'Enter' ? navigateTo('landing') : null}
-	}
+  // gå till landingpage
+  const onClickHome = () => {
+    console.log("jag vill hem!");
+    navigateTo("landing");
+  };
+  const onKeyDownHome = () => {
+    {
+      event.key === "Enter" ? navigateTo("landing") : null;
+    }
+  };
 
-	// gå till menyn
-	const onClickMenu = () => {
-		console.log('Jag vill se menyn');
-		navigateTo('menu')
-	}
-	const onKeyDownMenu = () => {
-		{event.key === 'Enter' ? navigateTo('menu') : null}
-	}
+  // gå till menyn
+  const onClickMenu = () => {
+    console.log("Jag vill se menyn");
+    navigateTo("menu");
+  };
+  const onKeyDownMenu = () => {
+    {
+      event.key === "Enter" ? navigateTo("menu") : null;
+    }
+  };
 
-	// gå till personal login
-	const onClickLogin = () => {
-		console.log('personal login');
-	}
-	const onKeyDownLogin = () => {
-		{event.key === 'Enter' ? console.log('personal login') : null}
-	}
+  // gå till personal login
+  const onClickLogin = () => {
+    console.log("personal login");
+  };
+  const onKeyDownLogin = () => {
+    {
+      event.key === "Enter" ? console.log("personal login") : null;
+    }
+  };
 
+  return (
+    <div className="header-desktop">
+      <img
+        src="src\components\images\logo-no-background 1.png"
+        alt="Företagslogga Feast & Fare"
+        onClick={onClickHome}
+        onKeyDown={onKeyDownHome}
+        className="header-logo"
+        tabIndex={0}
+      />
+      <ul className="main-menu  desktop-main-menu">
+        <li
+          onClick={onClickMenu}
+          onKeyDown={onKeyDownMenu}
+          className="link"
+          tabIndex={0}
+        >
+          {" "}
+          Till menyn
+        </li>
+        <li
+          onClick={onClickLogin}
+          onKeyDown={onKeyDownLogin}
+          className="link"
+          tabIndex={0}
+        >
+          Personal
+        </li>
+        <li className="phone">
+          <FontAwesomeIcon icon={faPhone} className="phone-icon" />
+          <p tabIndex={0}>0700-202020</p>
+        </li>
+      </ul>
+      <div>
+        <FontAwesomeIcon
+          icon={faShoppingBasket}
+          className={areItemsInCart ? "cart cart-desktop link fa-lg change-color " : "cart cart-desktop link fa-lg"}
+          onClick={onClickCart}
+          onKeyDown={onKeyDownCart}
+          aria-label="Varukorg"
+        />
+       <div>{areItemsInCart ? <TextForCartShopping /> : null}</div> 
+      </div>
+    </div>
+  );
+};
 
-	return(
-	<div className='header-desktop'>
-		<img src="src\components\images\logo-no-background 1.png" alt="Företagslogga Feast & Fare" onClick={onClickHome} onKeyDown={onKeyDownHome} className="header-logo" tabIndex={0}/>
-		<ul className="main-menu  desktop-main-menu" >
-			<li onClick ={onClickMenu} onKeyDown={onKeyDownMenu} className='link' tabIndex={0}> Till menyn</li>
-			<li onClick={onClickLogin} onKeyDown={onKeyDownLogin} className="link" tabIndex={0}>Personal</li>
-				<li className="phone">
-					<FontAwesomeIcon icon = {faPhone} className="phone-icon"/>
-					<p tabIndex={0}>0700-202020</p>
-				</li>
-			</ul>		
-			<FontAwesomeIcon icon = {faShoppingBasket} className='cart cart-desktop link fa-lg' onClick={onClickCart} onKeyDown={onKeyDownCart} aria-label='Varukorg' />
-
-			</div>
-	)		
-}
-
-
-export default LargeScreenHeader
+export default LargeScreenHeader;
