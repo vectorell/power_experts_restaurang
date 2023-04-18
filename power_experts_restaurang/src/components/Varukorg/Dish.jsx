1b7c00d9b109b7cab9f715dd95ac8b6306e50820
vectorell <vlindell@outlook.com>
2023-04-18 14:40:18 +0200

    Imported close-button

--- power_experts_restaurang/src/components/Varukorg/Dish.jsx
+++ power_experts_restaurang/src/components/Varukorg/Dish.jsx
@@ -1,6 +1,7 @@
 import './Varukorg.css'
 import { ContextProvider } from '../../App'
 import { useContext } from 'react'
+import closeIcon from "../images/close.png"
 
 function Dish({ name, description, price, onRemove }) {
 
@@ -9,7 +10,7 @@ function Dish({ name, description, price, onRemove }) {
             <h2 className="dish-name"> {name} </h2>
             <p className="dish-description"> {description} </p>
             <p className="dish-price"> {price}:- </p>
-            <img className="close-icon" src="./src/assets/close.png" onClick={onRemove} onKeyDown={onRemove} tabIndex={0} aria-label='Ta bort den valda maträtten'/>
+            <img className="close-icon" src={closeIcon} onClick={onRemove} onKeyDown={onRemove} tabIndex={0} aria-label='Ta bort den valda maträtten'/>
         </div>
     )
 }
